71c82cd9fbe8553e9af7aa5b6f5a64db5604bdf3
hemantttt727 <hemantsoni8005@gmail.com>
2025-06-27 13:46:22 +0530

    Create Profile.js

---
+++ Profile.js
@@ -0,0 +1,10 @@
+export default function Profile() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Your Profile</h1>
+      <p>Name: Hemant</p>
+      <p>Age: 14</p>
+      <p>Hobby: Coding, Badminton</p>
+    </div>
+  );
+}
