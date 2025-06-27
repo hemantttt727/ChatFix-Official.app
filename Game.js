73a9af9cfab70c90d49034f3430c6b70b48611b6
hemantttt727 <hemantsoni8005@gmail.com>
2025-06-27 13:46:57 +0530

    Create Game.js

---
+++ Game.js
@@ -0,0 +1,8 @@
+export default function Game() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Guess Partner’s Choice</h1>
+      <p>Game will launch soon...</p>
+    </div>
+  );
+}
