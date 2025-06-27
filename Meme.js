b996bddb4de5a918c02f732802500218c0b921fa
hemantttt727 <hemantsoni8005@gmail.com>
2025-06-27 13:45:44 +0530

    Create Meme.js

---
+++ Meme.js
@@ -0,0 +1,8 @@
+export default function Memes() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Trending Memes</h1>
+      <img src="https://i.imgur.com/JxOQf1v.jpeg" alt="Meme" width="300" />
+    </div>
+  );
+}
